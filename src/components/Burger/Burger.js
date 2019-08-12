4062c36b6bc616f2fdca2b0ac41fa34fd1b38988
Javier Celada <celada.jcm.javier@gmail.com>
2019-08-12 13:34:44 +0200

    Ingredients state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,15 +2,25 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = () => {
+const burger = (props) => {
+    const ingredients = adaptIngredients(props.ingredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
 };
+
+function adaptIngredients(ingredients) {
+    return Object.keys(ingredients)
+        .map(key => {
+            return [...Array(ingredients[key])]
+                .map((_, index) => {
+                    return <BurgerIngredient key={key + index} type={key}/>
+                })
+        });
+}
 
 export default burger;
