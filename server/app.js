be7380413782f6aee4da53caeda6fe5b83e3d338
Javier Celada <javier.celada@solera.com>
2019-12-02 10:21:47 +0100

    CORS has to be before defining routes

--- server/app.js
+++ server/app.js
@@ -14,35 +14,14 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// CORS
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/orders', routes);
 
 const port = 8080;
-
-// CORS
-const allowedOrigins = ['http://localhost:3000'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-  res.header("Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.listen(port, () => {
   console.log('Server is up and running on port ' + port);
 });
