b5173aa45c70656962c2ba0957ab863b6876f34e
Javier Celada <javier.celada@solera.com>
2019-12-16 16:09:07 +0100

    WIP: including router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder />
-        </Layout>
-      </div>
+        <BrowserRouter>
+          <div>
+            <Layout>
+              <BurgerBuilder/>
+            </Layout>
+          </div>
+        </BrowserRouter>
     );
   }
 }
