3a4a9970e44e12a336d277f1a4f63fdad5d0414f
Javier Celada <javier.celada@solera.com>
2019-12-16 14:18:31 +0100

    Errro handling cleaning

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,16 +7,24 @@ const withErrorHandler = (WrappedComponent, axios) => {
     state = {
       error: null
     };
+    requestInterceptor;
+    responseInterceptor;
 
     constructor(props) {
       super(props);
-      axios.interceptors.request.use(request => {
+      this.requestInterceptor = axios.interceptors.request.use(request => {
         this.setState({error: null});
         return request;
       });
-      axios.interceptors.response.use(response => response, error => {
-        this.setState({error: error})
-      });
+      this.responseInterceptor = axios.interceptors.response.use(
+          response => response, error => {
+            this.setState({error: error})
+          });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
